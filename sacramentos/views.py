604a683373cf65bc04b346c3e734fa2922beca59
luchitoflores <luchitoflores84@gmail.com>
2013-08-06 10:37:43 -0500

    Creación de un usuario con envio de datos a traves de peticiones POST

--- sacramentos/views.py
+++ sacramentos/views.py
@@ -18,29 +18,57 @@ from .forms import (
 
 
 
-def usuarioCreateView(request):
-	if request.is_ajax():
-		if request.method == 'POST':
-			valido = False
-			usuario_form = UsuarioForm(request.POST)
-			perfil_form = PerfilUsuarioForm(request.POST)
-			if usuario_form.is_valid() and perfil_form.is_valid():
-				valido = True
-				usuario = usuario_form.save(commit=False)
-				perfil = perfil_form.save(commit=False)
-				usuario.username = '%s%s%s' %(usuario.first_name, usuario.last_name, perfil.dni)
-				usuario.save()
-				perfil.user = usuario
-				perfil.save()
-				ctx = {'valido': valido}
+# def usuarioCreateView(request):
+# 	if request.is_ajax():
+# 		if request.method == 'POST':
+# 			valido = False
+# 			usuario_form = UsuarioForm(request.POST)
+# 			perfil_form = PerfilUsuarioForm(request.POST)
+# 			if usuario_form.is_valid() and perfil_form.is_valid():
+# 				valido = True
+# 				usuario = usuario_form.save(commit=False)
+# 				perfil = perfil_form.save(commit=False)
+# 				usuario.username = '%s%s%s' %(usuario.first_name, usuario.last_name, perfil.dni)
+# 				usuario.save()
+# 				perfil.user = usuario
+# 				perfil.save()
+# 				ctx = {'valido': valido}
 				
 
-			else:
-				errores_usuario = usuario_form.errors
-				errores_perfil =  perfil_form.errors
-				ctx = {'valido': valido, 'errores_usuario':errores_usuario, 'errores_perfil': errores_perfil}
+# 			else:
+# 				errores_usuario = usuario_form.errors
+# 				errores_perfil =  perfil_form.errors
+# 				ctx = {'valido': valido, 'errores_usuario':errores_usuario, 'errores_perfil': errores_perfil}
 
-			return HttpResponse(json.dumps(ctx), content_type='application/json')
+# 			return HttpResponse(json.dumps(ctx), content_type='application/json')
+# 	else:
+# 		usuario_form = UsuarioForm()
+# 		perfil_form = PerfilUsuarioForm()
+# 		ctx = {'usuario_form': usuario_form, 'perfil_form': perfil_form}
+# 		return render (request, 'usuario/usuario_form.html', ctx)
+
+def usuarioCreateView(request):
+	if request.method == 'POST':
+		valido = False
+		usuario_form = UsuarioForm(request.POST)
+		perfil_form = PerfilUsuarioForm(request.POST)
+		if usuario_form.is_valid() and perfil_form.is_valid():
+			valido = True
+			usuario = usuario_form.save(commit=False)
+			perfil = perfil_form.save(commit=False)
+			usuario.username = '%s%s%s' %(usuario.first_name, usuario.last_name, perfil.dni)
+			usuario.save()
+			perfil.user = usuario
+			perfil.save()
+			ctx = {'valido': valido}
+			return HttpResponseRedirect('/usuario/')
+			
+		else:
+			errores_usuario = usuario_form.errors
+			errores_perfil =  perfil_form.errors
+			messages.info(request, 'Errores al crear')
+			ctx = {'valido': valido, 'errores_usuario':errores_usuario, 'errores_perfil': errores_perfil}
+
 	else:
 		usuario_form = UsuarioForm()
 		perfil_form = PerfilUsuarioForm()
--- static/js/sacramentos.js
+++ static/js/sacramentos.js
@@ -6,7 +6,7 @@ function inicio(){
 	crear_padre($('#id_form_crear_madre'), '#id_madre','#id_crear_madre', 'Femenino');
 	autocomplete('#id_padre');
 	asignar_padre();
-	usuarioCreate();
+	// usuarioCreate();
 	tablas_estilo_bootstrap();
 	modelo_tablas('#id_table_libro, #id_table_feligres,#id_table_matrimonio,#id_table_bautismo,#id_table_eucaristia,#id_table_confirmacion');
 	campos_con_fechas();
@@ -75,46 +75,46 @@ function campos_con_fechas(){
 	
 }
 
-function usuarioCreate(){
-	$('#id_form_usuario_create').on('submit', function(e){
-		e.preventDefault();
-		json = $('#id_form_usuario_create').serialize();
-		url = '/usuario/add/';
-		$.post(url, json, function(data, status, jqXHR){
-			if(data.valido){
-				// $('#id_confirm_usuario_create').modal('show');
-				var mensaje = '<div class="alert alert-success">' + 
-				'<button type="button" class="close" data-dismiss="alert"><i class="icon-remove"></i></button>'+
-				'<img src="/static/img/success.png" alt=""> Usuario Creado exitosamente </div>';
-				$('#id_mensaje').html(mensaje);
-			} else {
-				var mensaje = '<div class="alert alert-error">' + 
-				'<button type="button" class="close" data-dismiss="alert"><i class="icon-remove"></i></button>'+
-				'<img src="/static/img/error.png" alt=""> Uno o más datos no son correctos </div>';
-				$('#id_mensaje').html(mensaje);
-				console.log(data.errores_usuario);
-				console.log(data.errores_perfil);
-				$.each(data.errores_usuario, function(index, element){
-					$("#id_"+index).addClass('invalid');
-					console.log("#id_"+index);
-					console.log("#id_"+element);
-					var mensajes_error = '<span>' + element+ '</span>';
-					console.log(mensajes_error);
-					$("#id_errors_"+index).append(mensajes_error);
-				});
-				console.log(data.errores_perfil);
-				$.each(data.errores_perfil, function(index, element){
-					$("#id_"+index).addClass('invalid');
-					console.log("#id_"+index);
-					console.log("#id_"+element);
-					var mensajes_error = '<span>' + element+ '</span>';
-					console.log(mensajes_error);
-					$("#id_errors_"+index).append(mensajes_error);
-				});
-			}
-		});
-});
-}
+// function usuarioCreate(){
+// 	$('#id_form_usuario_create').on('submit', function(e){
+// 		e.preventDefault();
+// 		json = $('#id_form_usuario_create').serialize();
+// 		url = '/usuario/add/';
+// 		$.post(url, json, function(data, status, jqXHR){
+// 			if(data.valido){
+// 				// $('#id_confirm_usuario_create').modal('show');
+// 				var mensaje = '<div class="alert alert-success">' + 
+// 				'<button type="button" class="close" data-dismiss="alert"><i class="icon-remove"></i></button>'+
+// 				'<img src="/static/img/success.png" alt=""> Usuario Creado exitosamente </div>';
+// 				$('#id_mensaje').html(mensaje);
+// 			} else {
+// 				var mensaje = '<div class="alert alert-error">' + 
+// 				'<button type="button" class="close" data-dismiss="alert"><i class="icon-remove"></i></button>'+
+// 				'<img src="/static/img/error.png" alt=""> Uno o más datos no son correctos </div>';
+// 				$('#id_mensaje').html(mensaje);
+// 				console.log(data.errores_usuario);
+// 				console.log(data.errores_perfil);
+// 				$.each(data.errores_usuario, function(index, element){
+// 					$("#id_"+index).addClass('invalid');
+// 					console.log("#id_"+index);
+// 					console.log("#id_"+element);
+// 					var mensajes_error = '<span>' + element+ '</span>';
+// 					console.log(mensajes_error);
+// 					$("#id_errors_"+index).append(mensajes_error);
+// 				});
+// 				console.log(data.errores_perfil);
+// 				$.each(data.errores_perfil, function(index, element){
+// 					$("#id_"+index).addClass('invalid');
+// 					console.log("#id_"+index);
+// 					console.log("#id_"+element);
+// 					var mensajes_error = '<span>' + element+ '</span>';
+// 					console.log(mensajes_error);
+// 					$("#id_errors_"+index).append(mensajes_error);
+// 				});
+// 			}
+// 		});
+// });
+// }
 
 function prueba(){
 	$('#id_form_busqueda').on('submit', function(e){

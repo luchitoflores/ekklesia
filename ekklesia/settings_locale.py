4597df4fc29f4fac2e475e710f4a14741046bd16
luchitoflores <luchitoflores84@gmail.com>
2013-10-01 00:37:55 -0500

    modulo dj-static

--- ekklesia/settings_locale.py
+++ ekklesia/settings_locale.py
@@ -1,32 +1,32 @@
 # -*- coding:utf-8 -*-
 # Django settings for ekklesia project.
-import dj_database_url
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIRECTORY = os.getcwd()
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Luis Flores', 'luchitoflores84@gmail.com'),
-    ('Edison Espinosa', 'edieslu@gmail.com'),
+    # ('Your Name', 'your_email@example.com'),
     )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-'default': dj_database_url.config()
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ekklesia.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
         }
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+        }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,11 +72,10 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # 'static',
+    'static',
     )
 
 # List of finder classes that know how to find static files in
@@ -134,7 +133,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django_extensions',
-    # 'storages',
 
     )
 
